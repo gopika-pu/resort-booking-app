fcd10c724c2ffff1f0419995161dad8e992cfa8e
gopika-pu <gopikapu2002@gmail.com>
2025-11-30 22:07:24 +0530

    front end code deployed

--- resort-backend/dist/controllers/booking.controller.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response } from "express";
-export declare const createBooking: (req: Request, res: Response) => Promise<void>;
-export declare const getBookings: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=booking.controller.d.ts.map
